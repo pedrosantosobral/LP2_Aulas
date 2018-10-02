96ccc194af2caf8440b81e2c74d77adcc28838b0
Pedro Santos <pedrosantosobral@gmail.com>
2018-10-02 11:04:32 +0100

    Primeira Aula

---
+++ MyAula01/MyFortnite/FNPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace MyFortnite
+{
+    public class FNPlayer
+    {
+        string EquipWeapon;
+        double HitPoints,Shield;
+
+        public void Atack(FNPlayer enemy)
+        {
+            enermy.TakeDamage(10);
+                          
+        }
+
+        public void TakeDamage(double damage)
+        {
+            if (Shield > 0)
+            {
+                Shield -= damage;
+                if (shield < 0)
+                {
+                    Shield = 0;
+                }
+
+            }
+        }
+            
+    }
+}
---
+++ MyAula01/MyFortnite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFortnite
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
